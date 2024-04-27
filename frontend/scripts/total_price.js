4d7d8ba28be848e9ac8de8f3293f46c260b406cf
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2024-04-27 20:33:22 +0300

    updated by adding buttons for increasing and decreasing the quantity of books in the cart

--- frontend/scripts/total_price.js
+++ frontend/scripts/total_price.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let bookPrice = parseFloat(document.getElementById("productPrice").innerText.replace("$", ""));
     let bookQuantity = document.getElementById("inputQuantity");
     let totalPriceElement = document.getElementById("totalPrice");
+    const increaseButton = document.getElementById("increaseQuantity");
+    const decreaseButton = document.getElementById("decreaseQuantity");
 
     function updatePrice(bookPrice, bookQuantity) {
         let quantity = bookQuantity.value;
@@ -15,6 +17,16 @@ document.addEventListener('DOMContentLoaded', function() {
     updatePrice(bookPrice, bookQuantity);
     bookQuantity.addEventListener("input", function()
     {
+        updatePrice(bookPrice, bookQuantity);
+    });
+
+    increaseButton.addEventListener("click", function() {
+        bookQuantity.value = parseInt(bookQuantity.value) + 1;
+        updatePrice(bookPrice, bookQuantity);
+    });
+
+    decreaseButton.addEventListener("click", function() {
+        bookQuantity.value = parseInt(bookQuantity.value) - 1;
         updatePrice(bookPrice, bookQuantity);
     });
 });
